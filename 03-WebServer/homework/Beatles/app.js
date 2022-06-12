083e6d7b2a1f01f4e55a52cdde9392e17d6dfadb
Cristian Fortich <yezidfortich@gmail.com>
2022-06-11 22:49:41 -0500

    solves homework 03

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs   = require('fs');
+const henryReadingTime = require('henry-reading-time');
 
 var beatles=[{
   name: "John Lennon",
@@ -21,4 +22,65 @@ var beatles=[{
   birthdate: "07/08/1940",
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
-]
+];
+
+http.createServer((req,res)=>{
+  let url = req.url;
+  if(req.url === '/'){
+    fs.readFile(`${__dirname}/index.html`,(err,data)=>{
+      if(err){
+        res.writeHead(404, {'Content-type':'text/plain'})
+        res.end('Error for some reason')
+      }else{
+        res.writeHead(200, {'Content-type':'text/html'})
+        res.end(data)
+      }
+    });
+  }else if(beatles.find(e=>url.replace('%20', ' ')=='/'+e.name)){
+    fs.readFile(`${__dirname}/beatle.html`,'utf-8',(err,data)=>{
+      if(err){
+        res.writeHead(404, {'Content-type':'text/plain'})
+        res.end('Error for some reason')
+      }else{
+        let person;
+        beatles.forEach(e=>{
+          if('/'+e.name==url.replace('%20', ' ')){
+            person =e;
+          }
+        });
+        res.writeHead(200, {'Content-type':'text/html'})
+        data = data.replaceAll('{name}',person.name);
+        data = data.replace('{birth}',person.birthdate);
+        data = data.replace('{profilePic}',person.profilePic);
+        res.end(data)
+      }
+    });
+  }else {
+    res.writeHead(404, {'Content-type':'text/plain'});
+    res.end('Beatle not found');
+  }
+
+  if(req.url === '/api'){
+    res.writeHead(200, {'Content-type':'application/json'});
+    res.end(JSON.stringify(beatles));
+  } else if(req.url.substring(0,5) === '/api/' && req.url.length >5){
+    let bName = req.url.split('/').pop();
+    if(beatles.find(e=>bName.replace('%20', ' ')==e.name)){
+      let name;
+      beatles.forEach(e=>{
+        if(e.name==bName.replace('%20', ' ')){
+          name =e;
+        }
+      });
+      res.writeHead(200, {'Content-type':'application.json'});
+      res.end(JSON.stringify(name));
+    }else {
+      res.writeHead(404, {'Content-type':'text/plain'});
+      res.end('Beatle not found');
+    }
+  }
+  // } else if(req.url === '/api/John%20Lennon'){
+  //   res.writeHead(200, {'Content-type':'application/json'});
+  //   res.end(JSON.stringify(beatles[0]));
+  // }
+}).listen(3000, '127.0.0.1');
